8b5140435d6597a00b66fa737cf400ac083e05f5
obaraelijah <elijahobara357@gmail.com>
2024-10-21 13:36:03 +0300

    tests: Buffer logic

--- pkg/io/memorybuffer_test.go
+++ pkg/io/memorybuffer_test.go
@@ -12,3 +12,113 @@ func Test_MemoryBuffer_InitialSizeZero(t *testing.T) {
 
 	assert.Equal(t, int64(0), b.Size())
 }
+
+func Test_MemoryBuffer_ReadAt_FromEmptyBuffer(t *testing.T) {
+	b := io.NewMemoryBuffer()
+	result := make([]byte, 64)
+
+	bytesRead, err := b.ReadAt(result, 0)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 0, bytesRead)
+}
+
+func Test_MemoryBuffer_Write_EmptyBuffer(t *testing.T) {
+	b := io.NewMemoryBuffer()
+
+	bytesWritten, err := b.Write([]byte(""))
+
+	assert.Nil(t, err)
+	assert.Equal(t, 0, bytesWritten)
+}
+
+func Test_MemoryBuffer_WriteAfterClose(t *testing.T) {
+	b := io.NewMemoryBuffer()
+
+	err := b.Close()
+	assert.Nil(t, err)
+
+	_, err = b.Write([]byte(""))
+
+	assert.Error(t, err)
+}
+
+func Test_MemoryBuffer_ReadAt_FromBeginning(t *testing.T) {
+	b := io.NewMemoryBuffer()
+	result := make([]byte, 5)
+
+	b.Write([]byte("abcdefghijklmnopqrstuvwxyz"))
+
+	bytesRead, err := b.ReadAt(result, 0)
+
+	assert.Nil(t, err)
+	assert.Equal(t, len(result), bytesRead)
+	assert.Equal(t, []byte("abcde"), result)
+}
+
+func Test_MemoryBuffer_ReadAt_FromMiddle(t *testing.T) {
+	b := io.NewMemoryBuffer()
+	result := make([]byte, 7)
+
+	b.Write([]byte("abcdefghijklmnopqrstuvwxyz"))
+
+	bytesRead, err := b.ReadAt(result, 2)
+
+	assert.Nil(t, err)
+	assert.Equal(t, len(result), bytesRead)
+	assert.Equal(t, []byte("cdefghi"), result)
+}
+
+func Test_MemoryBuffer_ReadAt_FromEnd(t *testing.T) {
+	b := io.NewMemoryBuffer()
+	result := make([]byte, 7)
+
+	b.Write([]byte("abcdefghijklmnopqrstuvwxyz"))
+
+	bytesRead, err := b.ReadAt(result, 26-3)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 3, bytesRead)
+	assert.Equal(t, []byte("xyz"), result[0:bytesRead])
+}
+
+func Test_MemoryBuffer_ReadAt_OffEnd(t *testing.T) {
+	b := io.NewMemoryBuffer()
+	result := make([]byte, 10)
+
+	b.Write([]byte("abcdefghijklmnopqrstuvwxyz"))
+
+	bytesRead, err := b.ReadAt(result, 26-3)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 3, bytesRead)
+	assert.Equal(t, []byte("xyz"), result[0:bytesRead])
+}
+
+func Test_MemoryBuffer_ReadAt_All(t *testing.T) {
+	b := io.NewMemoryBuffer()
+	content := []byte("abcdefghijklmnopqrstuvwxyz")
+	result := make([]byte, 26)
+
+	b.Write(content)
+
+	bytesRead, err := b.ReadAt(result, 0)
+
+	assert.Nil(t, err)
+	assert.Equal(t, len(content), bytesRead)
+	assert.Equal(t, content, result)
+}
+
+func Test_MemoryBuffer_ReadAt_EmptyBuffer(t *testing.T) {
+	b := io.NewMemoryBuffer()
+	content := []byte("abcdefghijklmnopqrstuvwxyz")
+	result := make([]byte, 0)
+
+	b.Write(content)
+
+	bytesRead, err := b.ReadAt(result, 0)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 0, bytesRead)
+	assert.Equal(t, 0, len(result))
+}
