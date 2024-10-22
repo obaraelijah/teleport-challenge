73f7dc68031d84e0e021251e8c7158fe224e72c8
obaraelijah <elijahobara357@gmail.com>
2024-10-22 11:56:50 +0300

    feat: block I/O controller implementation for cgroups

---
+++ pkg/cgroup/v1/blockio.go
@@ -0,0 +1,54 @@
+package cgroup
+
+import "github.com/obaraelijah/teleport-challenge/pkg/adaptation/os"
+
+const (
+	BlkioThrottleReadBpsDevice  = "blkio.throttle.read_bps_device"
+	BlkioThrottleWriteBpsDevice = "blkio.throttle.write_bps_device"
+)
+
+type blockIo struct {
+	base
+	readBpsDevice  *string
+	writeBpsDevice *string
+}
+
+func NewBlockIoController() *blockIo {
+	return NewBlockIoControllerDetailed(nil)
+}
+
+func NewBlockIoControllerDetailed(osAdapter *os.Adapter) *blockIo {
+	return &blockIo{
+		base: newBase("blkio", osAdapter),
+	}
+}
+
+func (b *blockIo) Apply(path string) error {
+	if b.readBpsDevice != nil {
+		if err := b.write([]byte(*b.readBpsDevice), "%s/%s", path, BlkioThrottleReadBpsDevice); err != nil {
+			return err
+		}
+	}
+
+	if b.writeBpsDevice != nil {
+		if err := b.write([]byte(*b.writeBpsDevice), "%s/%s", path, BlkioThrottleWriteBpsDevice); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Fluent interface for setting read limit
+func (b *blockIo) SetReadBpsDevice(value string) *blockIo {
+	b.readBpsDevice = &value
+
+	return b
+}
+
+// Fluent interface for setting write limit
+func (b *blockIo) SetWriteBpsDevice(value string) *blockIo {
+	b.writeBpsDevice = &value
+
+	return b
+}
