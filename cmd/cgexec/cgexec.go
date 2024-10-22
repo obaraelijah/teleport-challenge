a60da415bbdac9d6073239ac9cdbb387c88a7d72
obaraelijah <elijahobara357@gmail.com>
2024-10-22 12:46:42 +0300

    feat: Add cgexec helper program

---
+++ cmd/cgexec/cgexec.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/obaraelijah/teleport-challenge/pkg/command"
+)
+
+// main is the entrypoint of the cgexec application.  The application accepts
+// arguments in the form:
+//
+//	cgexec [<cgtskfile> ...] -- <command> [<arg> ...]
+//
+// Everything before the first "--" is treated as a cgroup task file; this
+// command will add itself to those cgroups.
+//
+// Everything after the first "--" is treated as the command and arguments to
+// the program to exec.
+//
+// If no "--" is found, then all arguments are treated as the command and
+// arguments to the program to exec.
+func main() {
+	if err := command.CgexecDetailed(os.Args, nil, nil); err != nil {
+		fmt.Printf("cgexec failed: %v", err)
+	}
+	// Cgexec shouldn't return in a non-error case
+	os.Exit(1)
+}
